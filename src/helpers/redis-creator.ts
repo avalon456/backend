2fbca24a14534a2f2e61d2d8bdeb5cfdf663b086
Iliyas Kazez <iliyas.kazez@dbt.asia>
2021-09-20 04:23:57 +0600

    Replace redis connection with RedisURL

--- src/helpers/redis-creator.ts
+++ src/helpers/redis-creator.ts
@@ -5,16 +5,16 @@ export function getRedisInstance(): any {
   
   const env: StringKV = process.env as any;
   const redisUri = env.REDIS_URL;
-  const redisHost = env.REDIS_HOST;
-  const redisPort = Number(env.REDIS_PORT);
-  const redisPassword = env.REDIS_PASSWORD;
+//   const redisHost = env.REDIS_HOST;
+//   const redisPort = Number(env.REDIS_PORT);
+//   const redisPassword = env.REDIS_PASSWORD;
   if (redisUri) {
-    // return  new IORedis(redisUri)
-    return new IORedis({
-      host: redisHost,
-      port: redisPort,
-      password: redisPassword,
-    });
+    return  new IORedis(redisUri)
+    // return new IORedis({
+    //   host: redisHost,
+    //   port: redisPort,
+    //   password: redisPassword,
+    // });
   } else {
     return new IORedis();
   }
